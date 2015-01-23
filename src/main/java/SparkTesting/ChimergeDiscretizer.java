bc94526b9ae64a0ea3700fc424500b098ea85c1f
Varsha Herle <rmysoreradhakrishna@expedia.com>
2015-01-22 21:43:28 -0800

    parallellize

--- src/main/java/SparkTesting/ChimergeDiscretizer.java
+++ src/main/java/SparkTesting/ChimergeDiscretizer.java
@@ -1,19 +1,21 @@
 package SparkTesting;
 
-import java.io.IOException;
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 
-import scala.Tuple2;
+import com.clearspring.analytics.util.Lists;
 
 
 /**
@@ -38,7 +40,7 @@ public class ChimergeDiscretizer implements Serializable {
 	
 	private static BigDecimal THRESHOLD = BigDecimal.valueOf(0.1d); //Default
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		Logger.getRootLogger().setLevel(Level.OFF);
 		Properties properties = Utils.loadProperties(args[0]);
 		if(properties == null) {
@@ -77,95 +79,24 @@ public class ChimergeDiscretizer implements Serializable {
 		});
 	    
 	    // Compute Chimerge for all attributes.
-
-	    //TODO: make this parallel. Tricky
-	    int numberOfAttributes = rawData.first().numberOfAttributes(); 
-	    for(int j = 0; j < numberOfAttributes; j++) {
-	    	
-		    //Step: Map raw line read from file to a AttributeLabelPair.
-		    JavaRDD<AttributeLabelPair> data = rawData.map(new RawDataToAttributeConverter(j, resolver, columns[j]));
-		    
-		    // Create a JavaPairRDD with attribute value, record itself.
-		    JavaPairRDD<BigDecimal, AttributeLabelPair> mapToPair = data.mapToPair(new RawDataToAttributePair());
-		    
-		    // Group by key to pull all records with same value together.
-		    JavaPairRDD<BigDecimal, Iterable<AttributeLabelPair>> groupByKey = mapToPair.groupByKey();
-		    
-		    // Now lets create a block which contains value and all its records which have that value. We need this for computing
-		    // Chisquare.
-		    JavaPairRDD<BigDecimal, Block> blocks = groupByKey.mapValues(new AttributeBlockCreator());
-		    
-		    BigDecimal min = BigDecimal.valueOf(Double.MIN_VALUE);
-			BigDecimal threshold = ChiSqaureTable.getChiSquareValue(resolver.getNumberOfClassLabels() - 1, THRESHOLD);
-			JavaRDD<Block> sourceRdd = null;
-			
-			while(min.compareTo(threshold) < 0) {
-			
-			    // Lets sort the blocks by attribute value.
-			    JavaPairRDD<BigDecimal, Block> sortedBlocksRdd = blocks.sortByKey(true);
-			    
-			    //Map Partitions With index
-			    JavaRDD<Tuple2<Integer, Tuple2<BigDecimal, Block>>> mapPartitionsWithIndex = 
-			    		sortedBlocksRdd.mapPartitionsWithIndex(new PartitionDataHandler(false), false);
-			    
-			    // We have not yet partitioned the data. We have just assigned each block to a partition number in the previous step.
-			    // The below step creates the partitions based on the partition number we assigned previously.
-			    JavaPairRDD<Integer, Tuple2<BigDecimal, Block>> mappedPartitions = mapPartitionsWithIndex
-			    		.mapToPair(new TupleToPair())
-				.partitionBy(
-					new SimplePartitioner(sortedBlocksRdd.partitions().size())
-				);
-			    
-			    // now create ChiSqUnit and compute chiSquare.
-			    JavaRDD<ChisquareUnit> chiSquaredRdd = mappedPartitions.mapPartitions(new DistributedChiSquareComputer());
-			    
-			    // Compute the Global minimum of the Chisquare and then merge the Blocks with the minimum value. 
-			    min = BigDecimal.valueOf(chiSquaredRdd.min(new ChiSquareUnitSorter()).getChiSquareValue());
-			    
-			    // Now lets return combined Block for all ChiSquareUnits whose value is equal to the minimum
-			    // Otherwise, return the two blocks separate without combining them.
-			    JavaRDD<Block> cm = chiSquaredRdd.mapPartitions(new BlockAggregator(min));
-			    
-			    // ******* begin loop : Merging/reducing  *******
-			    // Here the idea is that, we always move the first element from the partition to the previous partition.
-			    // i.e. move the first element from 1st partition to the oth partition. This way we give chance 
-			    // for the elements to merge across partitions.
-			    // the number of iterations  = partitions size. We need to give each partition a fair chance. 
-			    sourceRdd = cm;
-			    
-			    int i = 0;
-			    do {
-			    	i++;
-			    	sourceRdd = sourceRdd.mapPartitions(new BlockMergeHandler());
-			    	
-			    	sourceRdd = sourceRdd.mapPartitionsToPair(new BlockMapPartition())
-					.mapPartitionsWithIndex(new PartitionDataHandler(true), true)
-					.mapToPair(new TupleToPair())
-					.partitionBy(new SimplePartitioner(sourceRdd.partitions().size()))
-					.values()
-					.map(new Function<Tuple2<BigDecimal,Block>, Block>() {
-		
-						private static final long serialVersionUID = 1L;
-
-						public Block call(Tuple2<BigDecimal, Block> v1) throws Exception {
-							return v1._2();
-						}
-					});
-					
-			    } while(i < sourceRdd.partitions().size());
-			    // ******* end loop : Merging/reducing  *******
-			    
-			    // Here now all the Blocks with Chisquare = min have been merged.
-			    // Lets compute ChiSquare until Chisquare is greater than the allowed threshold
-			    blocks = sourceRdd.mapToPair(new BlockToPair());
-			    
-		    } // end of big while (Threshold value)
-	
-			Utils.printBlockRanges(sourceRdd);
-	    }
-	    System.out.println("Time to run: " + (System.currentTimeMillis() - startTime));	    
-	    jsc.stop();
+	    ExecutorService executorService  = Executors.newFixedThreadPool(10);
+	    List<Future<JavaRDD<Block>>> futureList = Lists.newArrayList();
 	    
+	    try {
+		    int numberOfAttributes = rawData.first().numberOfAttributes(); 
+		    for(int j = 0; j < numberOfAttributes; j++) {
+		    	futureList.add(executorService.submit(
+		    			new ChimergeRunner(rawData, resolver, columns[j], j, THRESHOLD)));
+		    }
+		    
+		    for(Future<JavaRDD<Block>> f: futureList) {
+		    	Utils.printBlockRanges(f.get());
+		    }
+	    } finally {
+	    	System.out.println("Time to run: " + (System.currentTimeMillis() - startTime));
+	    	executorService.shutdown();
+		    jsc.stop();
+	    }
 	}
 	
 	public static JavaSparkContext setupSpark(Properties properties) {
